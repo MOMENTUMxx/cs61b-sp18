ff4ad79d09c575ea3370a223f5b7c8eda6391da1
LujieWang <wanglujiexx@bupt.edu.cn>
2019-12-26 15:31:17 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int i = 0;
+        int sum = 0;
+        while (i < 10) {
+        	sum = sum + i;
+            System.out.print(sum + " ");
+            i = i + 1;
+        }
+    }
+}
