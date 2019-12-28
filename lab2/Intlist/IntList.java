f859cd9bcd52aae9e0a36564b4156ca096c1b0bf
LujieWang <wanglujiexx@bupt.edu.cn>
2019-12-28 22:26:56 +0800

    finish lab2 by WLJ

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,8 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null)
+            return B;
         IntList C = A;
         while (C.rest != null) {
             C = C.rest;
