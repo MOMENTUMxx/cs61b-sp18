64a8abf0196e30b7324dc4b8c3c7cc3b31b83aca
LujieWang <wanglujiexx@bupt.edu.cn>
2020-03-15 17:16:23 +0800

    hw2 finished by WLJ

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -4,7 +4,7 @@ import edu.princeton.cs.introcs.StdStats;
 import edu.princeton.cs.introcs.StdRandom;
 
 public class PercolationStats {
-    private int[] open;
+    private int[] threshold;
     private int temp;
     private int T;
     public PercolationStats(int N, int T, PercolationFactory pf) {
@@ -12,7 +12,7 @@ public class PercolationStats {
             throw new IllegalArgumentException();
         }
         this.T = T;
-        open = new int[T];
+        threshold = new int[T];
         for (int i = 0; i < T; i++) {
             Percolation p = pf.make(N);
             while (!p.percolates()) {
@@ -20,16 +20,16 @@ public class PercolationStats {
                 int y = StdRandom.uniform(N);
                 p.open(x, y);
             }
-            open[temp++] = p.numberOfOpenSites();
+            threshold[temp++] = p.numberOfOpenSites() / N;
         }
     }
 
     public double mean() {
-        return StdStats.mean(open);
+        return StdStats.mean(threshold);
     }
 
     public double stddev() {
-        return StdStats.stddev(open);
+        return StdStats.stddev(threshold);
     }
 
     public double confidenceLow() {
