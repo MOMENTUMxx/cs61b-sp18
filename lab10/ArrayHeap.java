6e16c95785b24abe6034f7a3a1b3baae9b9cfa8e
LujieWang <wanglujiexx@bupt.edu.cn>
2020-03-23 16:46:24 +0800

    lab10 finished by WLJ

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -195,7 +195,15 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
     public void changePriority(T item, double priority) {
         for (int i = 1; i <= size(); i++) {
             if (contents[i].item().equals(item)) {
+                double oldPriority = contents[i].priority();
                 contents[i].myPriority = priority;
+                if (priority > oldPriority) {
+                    sink(i);
+                } else if (priority < oldPriority) {
+                    swim(i);
+                } else {
+                    return;
+                }
                 return;
             }
         }
